b44006adb1ec80305df7eaf917ee00f8755708b6
pablo <pcoronel@untref.edu.ar>
2019-02-20 15:40:07 -0300

    server estatico

---
+++ 5_servir_archivos_estaticos/servir_archivos.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+// func holaMundo(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "<h1>Hola mundo</h1>")
+// }
+
+func prueba(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hola mundo desde /prueba</h1>")
+}
+
+func usuario(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hola mundo desde /usuario</h1>")
+}
+
+// Manejador (usa la funcion ServerHTTP)
+type mensaje struct {
+	msj string
+}
+
+// el recibidor asocia la funcion a la estructura "mensaje"
+func (m mensaje) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, m.msj)
+}
+
+func main() {
+	msj := mensaje{
+		msj: "Hola mundo de nuevo",
+	}
+
+	// crea el server mux
+	mux := http.NewServeMux()
+
+	// servir archivo estatico
+	// Todo lo que esté adentro de esta carpeta, va a estar disponible para la aplicacion (se puede poner js, css, img, etc)
+	fs := http.FileServer(http.Dir("public"))
+
+	mux.Handle("/", fs)
+	mux.HandleFunc("/prueba", prueba)
+	mux.HandleFunc("/usuario", usuario)
+
+	mux.Handle("/hola", msj)
+
+	// estructura server propia
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second, //tiempo de espera para escribir
+		WriteTimeout:   10 * time.Second, //tiempo de espera para leer antes de cortar la peticion
+		MaxHeaderBytes: 1 << 20,          //operador shift, multiplica por 2 el Nro de la izquierda (1) la cantidad de veces de la derecha (20)
+	}
+
+	log.Println("Escuchando...")
+	// server.ListenAndServe() //asi funciona
+
+	log.Fatal(server.ListenAndServe()) //asi corta el server si hay error
+}
